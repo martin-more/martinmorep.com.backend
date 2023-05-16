7f99f4bc46a12b83908e0d001ba760e5d17e9cc2
Martin More <martinmore@gmail.com>
2023-05-16 11:02:55 -0500

    minor: add clear_cache command

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 from flask.cli import FlaskGroup
 
-from app.main import app
+from app.main import app, cache
 
 cli = FlaskGroup(app)
+
+
+@cli.command("clear_cache")
+def create_db():
+    cache.clear()
 
 
 if __name__ == "__main__":
